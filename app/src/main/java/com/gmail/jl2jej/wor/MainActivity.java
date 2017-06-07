1f805726716a16da72b688fef00f982407d8a628
kido <jl2jej@gmail.com>
2017-06-08 05:50:39 +0900

    最初のバージョン カメラの有効、無効が切り替えられる

---
+++ app/src/main/java/com/gmail/jl2jej/wor/MainActivity.java
@@ -0,0 +1,65 @@
+package com.gmail.jl2jej.wor;
+
+import android.app.Activity;
+import android.app.admin.DevicePolicyManager;
+import android.content.Intent;
+import android.icu.text.DateFormat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.ComponentName;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.util.Log;
+
+
+public class MainActivity extends AppCompatActivity {
+    private final String TAG = "CameraDisable";
+    DevicePolicyManager devicePolicyManager;
+    ComponentName tCameraReceiver;
+    Boolean tCameraActive;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        devicePolicyManager = (DevicePolicyManager)getSystemService(this.DEVICE_POLICY_SERVICE);
+        tCameraReceiver = new ComponentName(this, cameraReceiver.class);
+
+        Intent intent = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+        intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, tCameraReceiver);
+        startActivityForResult(intent, 1);
+
+        tCameraActive = devicePolicyManager.isAdminActive(tCameraReceiver);
+
+        Switch directSwitch = (Switch)findViewById(R.id.directSwitch);
+        directSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked ) {
+                if (isChecked) {
+                    devicePolicyManager.setCameraDisabled(tCameraReceiver, true);
+                    Log.i(TAG, "true");
+                } else {
+                    devicePolicyManager.setCameraDisabled(tCameraReceiver, false);
+                    Log.i(TAG, "false");
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        switch (requestCode) {
+            case 1:
+                if (resultCode == Activity.RESULT_OK) {
+                    Log.i(TAG, "Administration enabled!");
+                } else {
+                    Log.i(TAG, "Administration enable FAILED!");
+                }
+                return;
+        }
+        super.onActivityResult(requestCode, resultCode, intent);
+    }
+
+}
