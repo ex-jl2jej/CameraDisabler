477d3678035686ded53479f87a1f07042f0b23cc
kido <jl2jej@gmail.com>
2017-06-11 10:20:37 +0900

    画面を作った あと、最初のところに前回有効化時刻を追加したい。

---
+++ app/src/main/java/com/gmail/jl2jej/wor/cameraReceiver.java
@@ -0,0 +1,20 @@
+package com.gmail.jl2jej.wor;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+/**
+ * Created by kido on 2017/06/05.
+ */
+
+public class cameraReceiver extends DeviceAdminReceiver {
+    @Override
+    public void onEnabled(Context context, Intent intent) {
+        //
+    }
+
+    @Override
+    public void onDisabled(Context context, Intent intent) {
+        //
+    }
+}
